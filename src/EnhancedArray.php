70f180c251b211ca0a0198dbf6a43785cc68f74c
Michael <maikdevelops@gmail.com>
2018-10-25 10:00:05 +0200

    Added extra features to the class.

--- src/EnhancedArray.php
+++ src/EnhancedArray.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace EnhancedArray;
+
+
 class EnhancedArray
 {
     private $values = [];
@@ -9,5 +12,76 @@ class EnhancedArray
         foreach( $params as $param ) {
             $this->values[] = $param;
         }
+    }
+
+
+    public function get( $index )
+    {
+        return $this->values[$index] ?? null;
+    }
+
+
+    public function getValues()
+    {
+        return $this->values;
+    }
+
+
+    public function addValue( $value )
+    {
+        $this->values[] = $value;
+
+        return $this;
+    }
+
+
+    public function addIndexedValue( $index, $value )
+    {
+        $this->values[$index] = $value;
+
+        return $this;
+    }
+
+
+    public function addValues( ...$params )
+    {
+        foreach( $params as $param ) {
+            $this->values[] = $param;
+        }
+
+
+        return $this;
+    }
+
+
+    public function addArrayValues( array $params )
+    {
+        foreach( $params as $param ) {
+            $this->addValue( $param );
+        }
+
+        return $this;
+    }
+
+
+    public function remove( $value )
+    {
+        if ( ! in_array( $value, $this->values ) ) {
+            throw new \Exception("The array doesn't contain the value {$value}");
+        }
+
+        unset( $this->values[ array_search($value, $this->values) ] );
+
+        return $this;
+    }
+
+
+    public function merge( EnhancedArray $enhancedArray )
+    {
+        foreach( $enhancedArray->getValues() as $value ) {
+            $this->addValue( $value );
+        }
+
+        return $this;
     }
 }
